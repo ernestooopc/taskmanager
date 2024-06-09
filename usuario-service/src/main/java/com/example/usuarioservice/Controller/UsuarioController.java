1c0d2063b86c79a15ccd1e93b101ee38ac6eb5d7
ernestooopc <ernestoo.pc@gmail.com>
2024-06-09 01:59:26 -0500

    Creacion de el model Usuarios para el microservicio Usuario-Service

---
+++ usuario-service/src/main/java/com/example/usuarioservice/Controller/UsuarioController.java
@@ -0,0 +1,32 @@
+package com.example.usuarioservice.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.usuarioservice.model.Usuario;
+import com.example.usuarioservice.repository.UsuarioRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario>getAllUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+     @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+}
