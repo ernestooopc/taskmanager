5096979d00bbf6655691a625f12cf5da937e6ad1
ernestooopc <ernestoo.pc@gmail.com>
2024-06-09 02:05:08 -0500

    Creacion de el Controlador y Repository del Usuario

--- usuario-service/src/main/java/com/example/usuarioservice/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.usuarioservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.usuarioservice.model.Usuario;
-
-public interface UsuarioRepository extends MongoRepository<Usuario,String>{
-
-}
