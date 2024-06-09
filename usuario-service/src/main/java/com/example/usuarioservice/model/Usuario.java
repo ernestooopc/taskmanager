82be6e3ca274d43e655514e30bf60e455f452831
ernestooopc <ernestoo.pc@gmail.com>
2024-06-09 01:58:30 -0500

    Creacion del Modelo Usuario del microservicio Usuario-service

---
+++ usuario-service/src/main/java/com/example/usuarioservice/model/Usuario.java
@@ -0,0 +1,48 @@
+package com.example.usuarioservice.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "usuarios")
+public class Usuario {
+    @Id
+    private String id;
+    private String nombre;
+    private String email;
+
+    // Constructor sin argumentos
+    public Usuario() {
+    }
+
+    // Constructor con argumentos
+    public Usuario(String id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
